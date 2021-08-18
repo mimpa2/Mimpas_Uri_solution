cd0ed5bf057d828c00ed6eb41af9f35749dd40a8
mimpa2 <mimpa.09@gmail.com>
2021-08-18 17:59:55 +0600

    Greastest

---
+++ src/TheG.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class TheG {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int ab = (a + b + Math.abs(a-b))/2;
+		int abc = (ab + c + Math.abs(ab - c))/2;
+		
+		System.out.print(abc+ " eh o maior  ");
+		
+		
+		
+	}
+
+}
