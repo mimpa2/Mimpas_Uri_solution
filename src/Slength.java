cc3e29373a1496d97b357ee0b03a9c2eb24c50de
mimpa2 <mimpa.09@gmail.com>
2021-08-18 21:56:43 +0600

    String

---
+++ src/Slength.java
@@ -0,0 +1,22 @@
+//import java.lang.*;
+import java.util.Scanner;
+public class Slength {
+
+	public static void main( String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String A = sc.next();
+		String B = sc.next();
+		
+	System.out.println(A.length() + B.length());
+		 if (A.compareTo(B) > 0 ) {
+			 
+			 System.out.println("Yes");
+		 }
+		 else {
+			 System.out.println("NO");
+		 }
+//System.out.println(A.substring);*/
+
+	}
+
+}
