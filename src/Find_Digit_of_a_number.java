9bc8d988718a05da7f3f42aba5c06e2588e6d114
mimpa2 <mimpa.09@gmail.com>
2021-08-18 22:52:28 +0600

    Find_Digit_of_A_Number

---
+++ src/Find_Digit_of_a_number.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Find_Digit_of_a_number {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int count = 1;
+		int modResult = 0;
+		
+		while (a >= 10) {
+			
+			modResult = a % 10;
+			count++;
+			a = a/10;
+			 System.out.println(modResult);
+			
+		}
+	
+		 System.out.println(a);
+		System.out.println("total num of digit : " +count); 
+		 
+	}
+	
+
+}
