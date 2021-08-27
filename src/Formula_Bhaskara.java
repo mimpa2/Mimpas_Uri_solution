89e2639a982853ac59004588dafbeb4248d5f8c1
mimpa2 <mimpa.09@gmail.com>
2021-08-28 00:22:31 +0600

    Edited Bhaskara_Formula

--- src/Formula_Bhaskara.java
+++ src/Formula_Bhaskara.java
@@ -9,20 +9,23 @@ public class Formula_Bhaskara {
 	double A = sc.nextDouble();
 	double B = sc.nextDouble();
 	double C = sc.nextDouble();
-	double R1 = 0;
-	double R2 = 0;
+	
 	if ((A == 0) || ((B*B) - (4*A*C) < 0)){
 		
-		System.out.println("Impossivel calcular”");
+		System.out.println("Impossivel calcular");
 	}
 	
 	else {
+		double R1 = 0;
+		double R2 = 0;
 		
-		R1 = ((-B + Math.sqrt(((B*B) - (4*A*C)))) / (2*A) );
-		R2 = ((-B - Math.sqrt(((B*B) - (4*A*C)))) / (2*A) );
+		
+		 R1 = ((-B + Math.sqrt(((B*B) - (4*A*C)))) / (2*A) );
+		 R2 = ((-B - Math.sqrt(((B*B) - (4*A*C)))) / (2*A) );
+		System.out.printf(" R1 = %.5f\n" , R1);
+		System.out.printf(" R2 = %.5f\n" , R2);
 	}
-System.out.printf(" R1 = %.5f\n" , R1);
-System.out.printf(" R2 = %.5f\n" , R2);
+
 	}
 
 }
