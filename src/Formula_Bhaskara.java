5010a4c6ff14607796ed86f0e03fb0fbca407164
mimpa2 <mimpa.09@gmail.com>
2021-08-27 13:14:21 +0600

    formula_Bhaskara

---
+++ src/Formula_Bhaskara.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Formula_Bhaskara {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	double A = sc.nextDouble();
+	double B = sc.nextDouble();
+	double C = sc.nextDouble();
+	double R1 = 0;
+	double R2 = 0;
+	if ((A == 0) || ((B*B) - (4*A*C) < 0)){
+		
+		System.out.println("Impossivel calcular”");
+	}
+	
+	else {
+		
+		R1 = ((-B + Math.sqrt(((B*B) - (4*A*C)))) / (2*A) );
+		R2 = ((-B - Math.sqrt(((B*B) - (4*A*C)))) / (2*A) );
+	}
+System.out.printf(" R1 = %.5f\n" , R1);
+System.out.printf(" R2 = %.5f\n" , R2);
+	}
+
+}
